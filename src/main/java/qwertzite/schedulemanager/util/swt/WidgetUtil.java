2b8d3145e7ab96fcc95ff28be5845efb88403cab
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-05-18 07:23:23 +0900

    メモ欄クラッシュ対応中

--- src/main/java/qwertzite/schedulemanager/util/swt/WidgetUtil.java
+++ src/main/java/qwertzite/schedulemanager/util/swt/WidgetUtil.java
@@ -19,14 +19,21 @@ public class WidgetUtil {
 			@Override
 			public void handleEvent(Event event) {
 				Text t = (Text) event.widget;
-				Rectangle r1 = t.getClientArea();
-				Rectangle r2 = t.computeTrim(r1.x, r1.y, r1.width, r1.height);
-				boolean hBar = (t.getStyle() & SWT.H_SCROLL) != 0;
-				boolean vBar = (t.getStyle() & SWT.V_SCROLL) != 0;
-				Point p = t.computeSize(!hBar ? r1.x : SWT.DEFAULT, !vBar ? r1.y : SWT.DEFAULT, true);
+				Rectangle r1 = t.getClientArea(); // a rectangle which describes the area of the receiver
+				Rectangle r2 = t.computeTrim(r1.x, r1.y, r1.width, r1.height); // bounding rectangle which would be required to produce that client area. 
+//				boolean hBar = (t.getStyle() & SWT.H_SCROLL) != 0;
+//				boolean vBar = (t.getStyle() & SWT.V_SCROLL) != 0;
+//				Point p = t.computeSize(!hBar ? r1.x : SWT.DEFAULT, !vBar ? r1.y : SWT.DEFAULT, true);
+				Point p = t.computeSize(SWT.DEFAULT, r1.height, true);
+				t.getHorizontalBar().setVisible(r1.width <= p.x);
+				r1 = t.getClientArea();
+				p = t.computeSize(r1.width, SWT.DEFAULT, true);
+				t.getVerticalBar().setVisible(r1.height <= p.y);
+				
 //				Point p = t.computeSize(r1.x, r1.y, true);
-				if (hBar) t.getHorizontalBar().setVisible(r2.width < p.x);
-				if (vBar) t.getVerticalBar().setVisible(r2.height < p.y);
+				System.out.println("r1=" + r1 + ",r2=" + r2 + ",p="+p + ",sh=" + (r1.width <= p.x) + ",sv=" + (r1.height <= p.y));
+//				if (hBar) 
+//				if (vBar)
 				if (event.type == SWT.Modify) {
 					t.getParent().layout(true);
 					t.showSelection();
