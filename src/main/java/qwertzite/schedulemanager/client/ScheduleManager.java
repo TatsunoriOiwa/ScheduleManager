284a4b920c0c1432a6bab6bbc8225c934d89dc06
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-05-26 21:36:28 +0900

    ver. 2.2.2, メモ欄クラッシュ問題解決

--- src/main/java/qwertzite/schedulemanager/client/ScheduleManager.java
+++ src/main/java/qwertzite/schedulemanager/client/ScheduleManager.java
@@ -15,8 +15,8 @@ public class ScheduleManager {
 
 	public static final String GAMENAME = "ScheduleManager";
 	public static final String GAME_ID = "schedulemanag";
-	public static final String VERSION = "2.2.1";
-	public static final String VERSION_SERIAL = "0017";
+	public static final String VERSION = "2.2.2";
+	public static final String VERSION_SERIAL = "0018";
 	
 	public static ScheduleManager INSTANCE = null;
 
--- src/main/java/qwertzite/schedulemanager/util/swt/WidgetUtil.java
+++ src/main/java/qwertzite/schedulemanager/util/swt/WidgetUtil.java
@@ -16,23 +16,31 @@ public class WidgetUtil {
 	 */
 	public static Text makeTextAutoScroll(Text text) {
 		Listener scrollBarListener = new Listener() {
+			private boolean ignore = false;
 			@Override
 			public void handleEvent(Event event) {
+				if (ignore) return;
 				Text t = (Text) event.widget;
 				Rectangle r1 = t.getClientArea(); // a rectangle which describes the area of the receiver
+				@SuppressWarnings("unused")
 				Rectangle r2 = t.computeTrim(r1.x, r1.y, r1.width, r1.height); // bounding rectangle which would be required to produce that client area. 
 //				boolean hBar = (t.getStyle() & SWT.H_SCROLL) != 0;
 //				boolean vBar = (t.getStyle() & SWT.V_SCROLL) != 0;
 //				Point p = t.computeSize(!hBar ? r1.x : SWT.DEFAULT, !vBar ? r1.y : SWT.DEFAULT, true);
 				Point p = t.computeSize(SWT.DEFAULT, r1.height, true);
+				this.ignore = true;
 				t.getHorizontalBar().setVisible(r1.width <= p.x);
+				this.ignore = false;
 				r1 = t.getClientArea();
 				p = t.computeSize(r1.width, SWT.DEFAULT, true);
+				this.ignore = true;
 				t.getVerticalBar().setVisible(r1.height <= p.y);
+				this.ignore = false;
 				
 //				Point p = t.computeSize(r1.x, r1.y, true);
-				System.out.println("r1=" + r1 + ",r2=" + r2 + ",p="+p + ",sh=" + (r1.width <= p.x) + ",sv=" + (r1.height <= p.y));
-//				if (hBar) 
+//				System.out.println("r1=" + r1 + ",r2=" + r2 + ",p="+p + ",sh=" + (r1.width <= p.x) + ",sv=" + (r1.height <= p.y));
+//				new Exception().printStackTrace();
+//				if (hBar)
 //				if (vBar)
 				if (event.type == SWT.Modify) {
 					t.getParent().layout(true);
