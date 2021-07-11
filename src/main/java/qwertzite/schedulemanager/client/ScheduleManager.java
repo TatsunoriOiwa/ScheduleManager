983c011211e9b559dd67bbd9bdd7d46b0dac7b5c
Oiwa <oiwa-tatsunori154@g.ecc.u-tokyo.ac.jp>
2021-07-11 11:05:04 +0900

    version 2.2.4

--- src/main/java/qwertzite/schedulemanager/client/ScheduleManager.java
+++ src/main/java/qwertzite/schedulemanager/client/ScheduleManager.java
@@ -15,8 +15,8 @@ public class ScheduleManager {
 
 	public static final String GAMENAME = "ScheduleManager";
 	public static final String GAME_ID = "schedulemanag";
-	public static final String VERSION = "2.2.3";
-	public static final String VERSION_SERIAL = "0019";
+	public static final String VERSION = "2.2.4";
+	public static final String VERSION_SERIAL = "0020";
 	
 	public static ScheduleManager INSTANCE = null;
 
